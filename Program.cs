1e7f78940e406a437b0244fd3ec617a76da10bae
Jose Lopez <joselopez89524@gmail.com>
2025-01-03 15:51:43 -0600

    Initialize OpenTK project with basic game window setup and add .gitignore

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+class Program
+{
+    static void Main()
+    {
+        var settings = GameWindowSettings.Default;
+        var nativeSettings = new NativeWindowSettings
+        {
+            Size = new Vector2i(800, 600),
+            Title = "OpenTK Game"
+        };
+
+        using (var window = new GameWindow(settings, nativeSettings))
+        {
+            window.RenderFrame += (FrameEventArgs args) =>
+            {
+                GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+                window.SwapBuffers();
+            };
+
+            window.Run();
+        }
+    }
+}
+
